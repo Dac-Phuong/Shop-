d76a63fe4cd79bb7df17516db9046ac827492c2c
Dac-Phuong <you@example.com>
2022-07-27 07:12:02 -0700

    upload

--- javascrip/register.js
+++ javascrip/register.js
@@ -3,12 +3,13 @@ function register(e) {
     var user_name = document.getElementById("user_name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+
     var user = {
         user_name : user_name,
         email : email,
         password : password,
     };
-
+  
     var jon = JSON.stringify(user);
     localStorage.setItem(user_name,jon);
     alert("Đăng ký thành công");
@@ -29,3 +30,31 @@ function login(e) {
         alert("Đăng nhập thất bại");
     }
 }
+//header
+window.onscroll = function() {myFunction()};
+    var header = document.getElementById("header__menu");
+    var sticky = header.offsetTop;
+    function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {  
+        header.classList.remove("sticky");
+    }
+}
+//Document
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
+  
